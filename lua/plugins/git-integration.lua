889226b0edaca73c2bdc841be19c5a81259798a9
kodiitulip <kodii.tulip@gmail.com>
2024-12-15 03:51:32 -0300

    Testing auth

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -3,7 +3,7 @@ return {
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require('gitsigns').setup()
-
+			-- Testing the git shit
 			vim.keymap.set('n', "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 			vim.keymap.set('n', "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
 		end,
