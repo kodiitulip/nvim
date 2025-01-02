ba3ea29b511c256bf37b9cdf84186979e4187baf
kodiitulip <kodii.tulip@gmail.com>
2025-01-02 12:11:40 -0300

    small cleaning

--- lua/kodie/plugins/lsp/lspconfig.lua
+++ lua/kodie/plugins/lsp/lspconfig.lua
@@ -12,6 +12,12 @@ return {
     local mason_lspconfig = require('mason-lspconfig')
     local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
+    -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+
     local keymap = vim.keymap
 
     vim.api.nvim_create_autocmd('LspAttach', {
--- lua/kodie/plugins/neo-tree.lua
+++ lua/kodie/plugins/neo-tree.lua
@@ -4,18 +4,57 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
+    '3rd/image.nvim',
+    {
+      's1n7ax/nvim-window-picker',
+      version = '2.*',
+      config = function()
+        require('window-picker').setup({
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', 'quickfix' },
+            },
+          },
+        })
+      end,
+    },
   },
   config = function()
-    -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
-    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
-    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
-    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
-
     require('neo-tree').setup({
       sort_case_insensitive = true, -- used when sorting files and directories in the tree
       filesystem = {
         group_empty_dirs = true, -- when true, empty folders will be grouped together
+      },
+      source_selector = {
+        winbar = true,
+        separator = { left = '/', right = '\\', override = 'right' },
+      },
+      window = {
+        mappings = {
+          ['P'] = { 'toggle_preview', config = { use_float = true, use_image_nvim = true } },
+        },
+      },
+      default_component_configs = {
+        diagnostics = {
+          symbols = {
+            hint = '󰌵 ',
+            info = ' ',
+            warn = ' ',
+            error = ' ',
+          },
+          highlights = {
+            hint = 'DiagnosticSignHint',
+            info = 'DiagnosticSignInfo',
+            warn = 'DiagnosticSignWarn',
+            error = 'DiagnosticSignError',
+          },
+        },
       },
     })
     vim.keymap.set('n', '<leader>ee', ':Neotree toggle<cr>', { desc = 'Toggle Neotree', silent = true })
