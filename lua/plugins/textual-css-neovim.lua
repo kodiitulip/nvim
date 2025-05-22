1454ed38d28e24a44b408a2f15f8f86cbcc52e90
kodiitulip <kodii.tulip@gmail.com>
2025-05-22 13:59:04 -0300

    feat: nvim-tcss -> code highlighter for textual css

---
+++ lua/plugins/textual-css-neovim.lua
@@ -0,0 +1,11 @@
+return {
+  { 'cachebag/nvim-tcss', config = true, ft = { 'tcss' } },
+  {
+    'echasnovski/mini.icons',
+    opts = {
+      filetype = {
+        tcss = { glyph = '󰙩', hl = 'MiniIconsYellow' },
+      },
+    },
+  },
+}
