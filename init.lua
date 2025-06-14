952a4b3483182849b48e05999e1ed60476fd3652
kodiitulip <kodii.tulip@gmail.com>
2025-06-14 09:35:31 -0300

    feat: adding godot dap adapters

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require('config.lazy')
+
+local dap = require('dap')
+dap.adapters.godot = {
+  type = 'server',
+  host = '127.0.0.1',
+  port = 6006,
+}
+dap.configurations.gdscript = {
+  {
+    type = 'godot',
+    request = 'launch',
+    name = 'Launch scene',
+    project = '${workspaceFolder}',
+  },
+}
