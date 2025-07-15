1026f2d5be5e728e44666395632554d6cd65eb28
kodiitulip <kodii.tulip@gmail.com>
2025-07-15 12:30:42 -0300

    feat: disabling spyglass and addign glyph for bolt files

---
+++ lua/plugins/mc-datapacks.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'echasnovski/mini.icons',
+    opts = {
+      filetype = {
+        mcfunction = { glyph = '󰍳', hl = 'MiniIconsGreen' },
+        bolt = { glyph = '', hl = 'MiniIconsLime' },
+      },
+      extension = {
+        mcfunction = { glyph = '󰍳', hl = 'MiniIconsGreen' },
+        mcmeta = { glyph = '󰍳', hl = 'MiniIconsLime' },
+        bolt = { glyph = '', hl = 'MiniIconsLime' },
+      },
+    },
+  },
+}
